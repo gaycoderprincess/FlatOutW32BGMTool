b27cee65002e2d13846baebbcc8ebfbb44531b82
💖 Chloe 💖 <gaycoderprincess@proton.me>
2024-07-25 17:27:19 +0200

    more detailed fbx export with dummies

--- main.cpp
+++ main.cpp
@@ -153,6 +153,7 @@ struct tSurface {
 	uint32_t nStreamId[2];
 	uint32_t nStreamOffset[2];
 
+	int _nFBXModelId;
 	int _nNumReferencesByType[NUM_SURFACE_REFERENCE_TYPES] = {};
 	int _nNumReferences = 0;
 	void RegisterReference(int type) {
@@ -1614,6 +1615,33 @@ bool CanSurfaceBeExported(tSurface* surface) {
 	return true;
 }
 
+int IsSurfaceValidAndExportable(int id) {
+	if (id < 0 || id >= aSurfaces.size()) return false;
+	if (!CanSurfaceBeExported(&aSurfaces[id])) return false;
+	return true;
+}
+
+aiNode* CreateNodeForTreeMesh(aiScene* scene, const tTreeMesh& treeMesh) {
+	int numTreeMeshes = 0;
+	if (IsSurfaceValidAndExportable(treeMesh.nSurfaceId2)) numTreeMeshes++;
+	if (IsSurfaceValidAndExportable(treeMesh.nSurfaceId3)) numTreeMeshes++;
+	if (IsSurfaceValidAndExportable(treeMesh.nSurfaceId4)) numTreeMeshes++;
+	if (IsSurfaceValidAndExportable(treeMesh.nSurfaceId5)) numTreeMeshes++;
+	if (numTreeMeshes <= 0) return nullptr;
+
+	auto node = new aiNode();
+	node->mName = "TreeMesh" + std::to_string(&treeMesh - &aTreeMeshes[0]);
+	node->mMeshes = new uint32_t[numTreeMeshes];
+	node->mNumMeshes = numTreeMeshes;
+
+	int i = 0;
+	if (IsSurfaceValidAndExportable(treeMesh.nSurfaceId2)) node->mMeshes[i++] = aSurfaces[treeMesh.nSurfaceId2]._nFBXModelId;
+	if (IsSurfaceValidAndExportable(treeMesh.nSurfaceId3)) node->mMeshes[i++] = aSurfaces[treeMesh.nSurfaceId3]._nFBXModelId;
+	if (IsSurfaceValidAndExportable(treeMesh.nSurfaceId4)) node->mMeshes[i++] = aSurfaces[treeMesh.nSurfaceId4]._nFBXModelId;
+	if (IsSurfaceValidAndExportable(treeMesh.nSurfaceId5)) node->mMeshes[i++] = aSurfaces[treeMesh.nSurfaceId5]._nFBXModelId;
+	return node;
+}
+
 aiScene GenerateScene() {
 	aiScene scene;
 	scene.mRootNode = new aiNode();
@@ -1661,16 +1689,15 @@ aiScene GenerateScene() {
 
 	scene.mMeshes = new aiMesh*[numSurfaces];
 	scene.mNumMeshes = numSurfaces;
-	scene.mRootNode->mMeshes = new uint32_t[numSurfaces];
-	scene.mRootNode->mNumMeshes = numSurfaces;
+
 	int counter = 0;
 	for (auto& src : aSurfaces) {
 		if (!CanSurfaceBeExported(&src)) continue;
 
 		int i = counter++;
+		src._nFBXModelId = i;
 
 		scene.mMeshes[i] = new aiMesh();
-		scene.mRootNode->mMeshes[i] = i;
 
 		auto dest = scene.mMeshes[i];
 		dest->mMaterialIndex = src.nMaterialId;
@@ -1796,6 +1823,99 @@ aiScene GenerateScene() {
 				dest->mFaces[j].mIndices[2] = indices[2];
 				dest->mFaces[j].mNumIndices = 3;
 				indexData += 2 * 3;
+			}
+		}
+	}
+
+	if (auto node = new aiNode()) {
+		int numStaticMeshes = 0;
+		for (auto& batch : aStaticBatches) {
+			if (IsSurfaceValidAndExportable(batch.nSurfaceId)) numStaticMeshes++;
+		}
+
+		node->mName = "StaticBatch";
+		scene.mRootNode->addChildren(1, &node);
+		node->mMeshes = new uint32_t[numStaticMeshes];
+		node->mNumMeshes = numStaticMeshes;
+		int i = 0;
+		for (auto &batch: aStaticBatches) {
+			if (!IsSurfaceValidAndExportable(batch.nSurfaceId)) continue;
+			node->mMeshes[i++] = aSurfaces[batch.nSurfaceId]._nFBXModelId;
+		}
+	}
+
+	if (auto node = new aiNode()) {
+		node->mName = "TreeMesh";
+		scene.mRootNode->addChildren(1, &node);
+		for (auto& treeMesh : aTreeMeshes) {
+			if (auto treeNode = CreateNodeForTreeMesh(&scene, treeMesh)) {
+				node->addChildren(1, &treeNode);
+			}
+		}
+	}
+
+	if (auto node = new aiNode()) {
+		node->mName = "Objects";
+		scene.mRootNode->addChildren(1, &node);
+		for (auto& object : aObjects) {
+			auto objectNode = new aiNode();
+			node->addChildren(1, &objectNode);
+
+			objectNode->mName = object.sName1;
+			objectNode->mTransformation.a1 = object.mMatrix[0];
+			objectNode->mTransformation.b1 = object.mMatrix[1];
+			objectNode->mTransformation.c1 = object.mMatrix[2];
+			objectNode->mTransformation.d1 = object.mMatrix[3];
+			objectNode->mTransformation.a2 = object.mMatrix[4];
+			objectNode->mTransformation.b2 = object.mMatrix[5];
+			objectNode->mTransformation.c2 = object.mMatrix[6];
+			objectNode->mTransformation.d2 = object.mMatrix[7];
+			objectNode->mTransformation.a3 = object.mMatrix[8];
+			objectNode->mTransformation.b3 = object.mMatrix[9];
+			objectNode->mTransformation.c3 = object.mMatrix[10];
+			objectNode->mTransformation.d3 = object.mMatrix[11];
+			objectNode->mTransformation.a4 = object.mMatrix[12];
+			objectNode->mTransformation.b4 = object.mMatrix[13];
+			objectNode->mTransformation.c4 = object.mMatrix[14];
+			objectNode->mTransformation.d4 = object.mMatrix[15];
+		}
+	}
+
+	if (auto node = new aiNode()) {
+		node->mName = "CompactMesh";
+		scene.mRootNode->addChildren(1, &node);
+		for (auto& compactMesh : aCompactMeshes) {
+			auto meshNode = new aiNode();
+			meshNode->mName = compactMesh.sName1;
+			meshNode->mTransformation.a1 = compactMesh.mMatrix[0];
+			meshNode->mTransformation.b1 = compactMesh.mMatrix[1];
+			meshNode->mTransformation.c1 = compactMesh.mMatrix[2];
+			meshNode->mTransformation.d1 = compactMesh.mMatrix[3];
+			meshNode->mTransformation.a2 = compactMesh.mMatrix[4];
+			meshNode->mTransformation.b2 = compactMesh.mMatrix[5];
+			meshNode->mTransformation.c2 = compactMesh.mMatrix[6];
+			meshNode->mTransformation.d2 = compactMesh.mMatrix[7];
+			meshNode->mTransformation.a3 = compactMesh.mMatrix[8];
+			meshNode->mTransformation.b3 = compactMesh.mMatrix[9];
+			meshNode->mTransformation.c3 = compactMesh.mMatrix[10];
+			meshNode->mTransformation.d3 = compactMesh.mMatrix[11];
+			meshNode->mTransformation.a4 = compactMesh.mMatrix[12];
+			meshNode->mTransformation.b4 = compactMesh.mMatrix[13];
+			meshNode->mTransformation.c4 = compactMesh.mMatrix[14];
+			meshNode->mTransformation.d4 = compactMesh.mMatrix[15];
+			node->addChildren(1, &meshNode);
+
+			// not loading the damaged or lod parts here
+			if (!compactMesh.aLODMeshIds.empty()) {
+				auto model = aModels[compactMesh.aLODMeshIds[0]];
+				std::vector<int> aMeshes;
+				for (auto& surfaceId : model.aSurfaces) {
+					if (!IsSurfaceValidAndExportable(surfaceId)) continue;
+					aMeshes.push_back(aSurfaces[surfaceId]._nFBXModelId);
+				}
+				meshNode->mMeshes = new uint32_t[aMeshes.size()];
+				memcpy(meshNode->mMeshes, &aMeshes[0], aMeshes.size() * sizeof(uint32_t));
+				meshNode->mNumMeshes = aMeshes.size();
 			}
 		}
 	}
